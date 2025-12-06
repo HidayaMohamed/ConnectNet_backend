18a2661c30adc7ff908a46120a7c9bd220a7acfd
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-12-06 21:55:06 +0300

    Fixed relative import in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI
-from .routers import users, auth, posts, comments, likes, follows
+from routers import users, auth, posts, comments, likes, follows
 
 app = FastAPI(title="ConnectNet")
 
