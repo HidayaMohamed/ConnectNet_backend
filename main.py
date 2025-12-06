e8b11760bf3f392ae76b5ee495b7b7f494ff85ff
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-12-06 12:16:44 +0300

    Updated main.py to run all the classes

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 
 from fastapi import FastAPI
-from .routers import users
+from .routers import users,posts, comments, likes, follows
 
 app = FastAPI(title="ConnectNet")
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(posts.router, prefix="/posts", tags=["Posts"])
+app.include_router(comments.router, prefix="/comments", tags=["Comments"])
+app.include_router(likes.router, prefix="/likes", tags=["Likes"])
+app.include_router(follows.router, prefix="/follows", tags=["Follows"])
 
 @app.get("/")
 def root():
